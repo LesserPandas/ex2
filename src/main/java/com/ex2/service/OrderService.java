772c39be7ba27568e7638ebed86103a95e87598f
less3rPanda <86329285+LesserPandas@users.noreply.github.com>
2022-04-17 23:20:41 +0900

    Ch7 Order
    Order Create

---
+++ src/main/java/com/ex2/service/OrderService.java
@@ -0,0 +1,43 @@
+package com.ex2.service;
+
+import com.ex2.dto.OrderDto;
+import com.ex2.entity.Item;
+import com.ex2.entity.Member;
+import com.ex2.entity.Order;
+import com.ex2.entity.OrderItem;
+import com.ex2.repository.ItemRepository;
+import com.ex2.repository.MemberRepository;
+import com.ex2.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+
+    public Long order(OrderDto orderDto, String email){
+        Item item = itemRepository.findById(orderDto.getItemId())
+                .orElseThrow(EntityNotFoundException::new);
+        Member member = memberRepository.findByEmail(email);
+
+        List<OrderItem> orderItemList = new ArrayList<>();
+        OrderItem orderItem =
+                OrderItem.createOrderItem(item, orderDto.getCount());
+        orderItemList.add(orderItem);
+
+        Order order = Order.createOrder(member, orderItemList);
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+}
